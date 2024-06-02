da3606e4cc3b57d9cf3adefc3421013213429bd2
ranim-taboush <ranim.taboush@gmail.com>
2024-06-02 10:57:39 +0300

    Initial commit from Create Next App

---
+++ src/app/page.js
@@ -0,0 +1,19 @@
+
+export default function Home() {
+  
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <iframe src="https://app.lapentor.com/sphere/demo-1717309585" 
+      frameBorder="0" 
+      width="100%" 
+      height="400px" 
+      scrolling="no" 
+      allow="vr,gyroscope,accelerometer" 
+      allowFullScreen="true" 
+      webkitAllowFullScreen="true" 
+      mozAllowFullScreen="true" 
+      oAllowFullScreen="true" 
+      msAllowFullScreen="true"></iframe>
+    </main>
+  );
+}
