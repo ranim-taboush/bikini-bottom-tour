fec87244f22e6754b0195effe7fd82dcf916468d
ranim-taboush <ranim.taboush@gmail.com>
2024-06-02 11:40:19 +0300

    iframe

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,10 @@
 
 export default function Home() {
-  
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <iframe src="https://app.lapentor.com/sphere/demo-1717309585" 
+      <iframe src="https://app.lapentor.com/sphere/demo-1717309585"
       frameBorder="0" 
-      width="100%" 
+      width="800px" 
       height="400px" 
       scrolling="no" 
       allow="vr,gyroscope,accelerometer" 
